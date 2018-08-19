70931ecd7079879e0d2f913f187ed5472ea2ebbe
Glendleton <gpendlet92@gmail.com>
2018-08-19 14:27:15 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Discussion Questions
+// 1)string.toLowerCase()
+// 2)you can make data match by making it all lower case.
+// 3)it removes extra spaces on either side of your code
+// 4)anytime you want to remove extra spaces
+// 5)you can split a string into another array, or join elements into a string
+// 6)it validates data
+// 7)a backslash
+// 8)you can test for whichever elements you're looking for
+// var str = "The best things in life are free";
+// var patt = new RegExp("e");
+// var res = patt.test(str);
+
+// begin step 4)Create an array of 5 strings. Make sure to capitalize some of the letters in your strings.
+
+// step 5)Loop through the array. Change each string to lowercase, and then add them to a new array.
+
+// step 6)Loop through the new array (all lowercase strings) and log the first and last character of each string to the console.
+
+// step 7)Create a variable and store your name in it. Using the split method and a loop, log each letter of your name to the console.
+
+// step 8)Using the join method, put your name back together and replace all occurrences of one letter with another. (ie: replace all a's with y's)
+
+// step 10)Create a function that will get the user's input, test it against the standard regular expression for emails, and log the result in the console.
+
+// step 11)make the function "listen" to the button using addEventListener().
